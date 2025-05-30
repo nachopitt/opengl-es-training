81388e5391760de0146e8121b9533e180db779f4
nachopitt <nachopitt@gmail.com>
2025-05-30 02:33:18 -0600

    Add Engine header file placeholder

---
+++ rendix/include/core/Engine.h
@@ -0,0 +1,13 @@
+#ifndef RENDIX_CORE_ENGINE
+#define RENDIX_CORE_ENGINE
+
+namespace rendix::core
+{
+    class Engine
+    {
+        public:
+            void run();
+    }
+}
+
+#endif // RENDIX_CORE_ENGINE
